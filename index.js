0dd128b24527556fa8db2ad550d5490de48f1f43
gtjames <gtjames@gmail.com>
2022-08-01 16:29:27 -0500

    cleaned up a few warnings is all

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 //  a pretty good but circuitous route to get to this point
 //  read the article but reference this code
 
-const casblanca = require('./casablanca.json');     //  everyones favorite package
+// const casblanca = require('./casablanca.json');     //  everyones favorite package
 
-const express = require('express');     //  everyones favorite package
-const app = express()                   //  make this code an express router
-const server = require('http').createServer(app)
-const port = process.env.PORT || 3000   //  setting up a server on port 3000
-const io = require('socket.io')(server) //  create the socket handler and tell it who our server is
-const path = require('path')            //  path package knows about the server environment the code is running on
+const express = require('express');      //  everyone's favorite package
+const app = express();                   //  make this code an express router
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;   //  setting up a server on port 3000
+const io = require('socket.io')(server); //  create the socket handler and tell it who our server is
+const path = require('path');            //  path package knows about the server environment the code is running on
 
 //    the use method is how a URL path is associated with a JS file to handle browser requests
 //    use has two params (usually) param one is the path for the URL and param 2 is the router to be called
@@ -30,11 +30,11 @@ io.on('connection', socket => {             //  io is our socket handler. When t
     });
 });
 
-function showQuote() {                      //  timer method that kicks off regularly
-  let rand = Math.floor(Math.random() * casblanca.quotes.length)
-  let message = casblanca.quotes[rand].quote;   //  find a random quote
-  io.emit('chat', message);                 //  send the quote to all listeners
-}  
+// function showQuote() {                      //  timer method that kicks off regularly
+//   let rand = Math.floor(Math.random() * casblanca.quotes.length)
+//   let message = casblanca.quotes[rand].quote;   //  find a random quote
+//   io.emit('chat', message);                 //  send the quote to all listeners
+// }
 
 //define time interval and call user-defined waitAndshow function
 //  setInterval(showQuote, 4000);
